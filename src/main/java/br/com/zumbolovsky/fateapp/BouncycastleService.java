5cc9814d51a7f9041342423c3bee3bb3628d4fe7
Andrew Siquieri <andrew.siquieri@sinqia.com.br>
2022-01-26 11:12:21 -0300

    Creating BouncyCastle RSA key generation, signature and verification

---
+++ src/main/java/br/com/zumbolovsky/fateapp/BouncycastleService.java
@@ -0,0 +1,72 @@
+package br.com.zumbolovsky.fateapp;
+
+import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.Signature;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BouncycastleService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BouncycastleService.class);
+
+    public static void main(final String[] args) {
+        Security.addProvider(new BouncyCastleProvider());
+        final KeyPair keyPair = generateKeyPair();
+        final String base64EncodedText = "Base64EncodedText";
+        final byte[] signature = sign(keyPair, base64EncodedText);
+        final boolean verified = verify(keyPair, base64EncodedText, signature);
+        if(verified) {
+            LOGGER.info("Sign successful! Verified!");
+        } else {
+            LOGGER.info("Sign unsuccessful! Not verified!");
+        }
+    }
+
+    private static KeyPair generateKeyPair() {
+        final KeyPairGenerator generator = getKeyPairGenerator();
+        generator.initialize(2048, new SecureRandom());
+        return generator.generateKeyPair();
+    }
+    private static KeyPairGenerator getKeyPairGenerator() {
+        try {
+            return KeyPairGenerator.getInstance("RSA", "BC");
+        } catch(final Exception ex) {
+            throw new RuntimeException("No such algorithm or provider for key pair generation!", ex);
+        }
+    }
+
+    private static byte[] sign(final KeyPair keyPair, final String base64EncodedText) {
+        final Signature signer = getSigner();
+        try {
+            signer.initSign(keyPair.getPrivate());
+            signer.update(base64EncodedText.getBytes(StandardCharsets.UTF_8));
+            return signer.sign();
+        } catch (final Exception ex) {
+            throw new RuntimeException("Cannot generate RSA sigature.", ex);
+        }
+    }
+
+    private static boolean verify(final KeyPair keyPair, final String base64EncodedText, final byte[] signature) {
+        final Signature signer = getSigner();
+        try {
+            signer.initVerify(keyPair.getPublic());
+            signer.update(base64EncodedText.getBytes(StandardCharsets.UTF_8));
+            return signer.verify(signature);
+        } catch (final Exception ex) {
+            throw new RuntimeException("Cannot verify RSA sigature.", ex);
+        }
+    }
+
+    private static Signature getSigner() {
+        try {
+            return Signature.getInstance("SHA512withRSA", "BC");
+        } catch(final Exception ex) {
+            throw new RuntimeException("No such algorithm or provider for signer!", ex);
+        }
+    }
+}
