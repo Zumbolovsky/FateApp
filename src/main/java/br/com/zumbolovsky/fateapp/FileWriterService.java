e318c2ed69b2a33791856b2ee8abdbe50281d28f
Andrew Siquieri <andrew.siquieri@sinqia.com.br>
2022-01-27 15:34:50 -0300

    Creating and fixing file reading and writing tests

--- src/main/java/br/com/zumbolovsky/fateapp/FileWriterService.java
+++ src/main/java/br/com/zumbolovsky/fateapp/FileWriterService.java
@@ -3,22 +3,36 @@ package br.com.zumbolovsky.fateapp;
 import static java.nio.file.StandardOpenOption.APPEND;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.google.common.primitives.Longs;
 
 public class FileWriterService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileWriterService.class);
+
+    /**
+     * {@code Paths.get(String, String...)} works with absolute or relative paths:
+     * <br>
+     * - For absolute paths (Linux example): /home/andrew-siquieri/test.txt
+     * <br>
+     * - For relative paths (project source directory): test.txt
+     * */
     public static void main(final String[] args) {
         try {
+            final Path readPath = Paths.get("test.txt");
+            LOGGER.info("Text contained in the file is: {}", Files.readString(readPath));
             byte b = 1;
-            long l = System.currentTimeMillis( );
-            Path path = Paths.get("test.du");
-            Files.write(path, new byte[]{b});
-            Files.write(path, Longs.toByteArray(l), APPEND);
-            Files.write(path, "Text".getBytes(StandardCharsets.UTF_8), APPEND);
+            long l = System.currentTimeMillis();
+            //The resulting file won't be readable because it is a bunch of bytes
+            final Path writePath = Paths.get("result.txt");
+            Files.deleteIfExists(writePath);
+            Files.write(writePath, new byte[]{b});
+            Files.write(writePath, Longs.toByteArray(l), APPEND);
+            Files.write(writePath, "Text".getBytes(), APPEND);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
