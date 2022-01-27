ec9cb0e1584ca215880bdd6a817c518fb70b55e9
Andrew Siquieri <andrew.siquieri@sinqia.com.br>
2022-01-27 12:29:49 -0300

    Creating and fixing YAML file reading and writing as string test

--- src/main/java/br/com/zumbolovsky/fateapp/YamlReader.java
+++ src/main/java/br/com/zumbolovsky/fateapp/YamlReader.java
@@ -2,6 +2,7 @@ package br.com.zumbolovsky.fateapp;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,11 +17,21 @@ public class YamlReader {
     public YamlReader() {
         objectMapper = new ObjectMapper(new YAMLFactory());
         objectMapper.findAndRegisterModules();
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
     }
 
     public TestObject read(final InputStream inputStream) {
         try {
             return objectMapper.readValue(inputStream, TestObject.class);
+        } catch(final IOException e) {
+            logger.info("Invalid read on YAML file!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String readAsString(final InputStream inputStream) {
+        try {
+            return objectMapper.writeValueAsString(read(inputStream));
         } catch(final IOException e) {
             logger.info("Invalid read on YAML file!");
             throw new RuntimeException(e);
--- src/main/java/br/com/zumbolovsky/fateapp/YamlTestService.java
+++ src/main/java/br/com/zumbolovsky/fateapp/YamlTestService.java
@@ -1,7 +1,9 @@
 package br.com.zumbolovsky.fateapp;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,10 +13,14 @@ public class YamlTestService {
 
     public static void main(final String[] args) {
         final YamlReader reader = new YamlReader();
-        try {
-            final FileInputStream inputStream = new FileInputStream("src/main/resources/test.yml");
-            LOGGER.info("File read outputs class as: \n{}", reader.read(inputStream));
-        } catch(final FileNotFoundException e) {
+        readAndExecute(inputStream -> LOGGER.info("File read outputs class as: \n{}", reader.read(inputStream)));
+        readAndExecute(inputStream -> LOGGER.info("File read outputs string as: \n{}", reader.readAsString(inputStream)));
+    }
+
+    private static void readAndExecute(final Consumer<InputStream> consumer) {
+        try (FileInputStream inputStream = new FileInputStream("src/main/resources/test.yml")) {
+            consumer.accept(inputStream);
+        } catch(final IOException e) {
             LOGGER.info("File not found");
             throw new RuntimeException(e);
         }
