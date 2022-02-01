f2fe34e2431bd456b99be947f2b553b8bcc38854
Zumbolovsky <a.siquieri@hotmail.com>
2022-02-01 01:38:38 -0300

    Creating and configuring OAuth2 Google Sign-in example

--- src/main/kotlin/br/com/zumbolovsky/fateapp/FateAppApplication.kt
+++ src/main/kotlin/br/com/zumbolovsky/fateapp/FateAppApplication.kt
@@ -1,6 +1,5 @@
 package br.com.zumbolovsky.fateapp
 
-import br.com.zumbolovsky.fateapp.config.security.SecurityConfig.SecurityConstants.HEADER_NAME
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
@@ -25,12 +24,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy
         version = "v0.0.1",
         license = License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0"),
         contact = Contact(name = "Andrew Siquieri")))
-@SecurityScheme(
-    name = HEADER_NAME,
-    scheme = "basic",
-    type = SecuritySchemeType.APIKEY,
-    `in` = SecuritySchemeIn.HEADER,
-    bearerFormat = "JWT")
 class FateAppApplication {
 
     companion object {
--- src/main/kotlin/br/com/zumbolovsky/fateapp/config/security/SecurityConfig.kt
+++ src/main/kotlin/br/com/zumbolovsky/fateapp/config/security/SecurityConfig.kt
@@ -1,65 +1,24 @@
 package br.com.zumbolovsky.fateapp.config.security
 
-import br.com.zumbolovsky.fateapp.service.UserService
 import org.springframework.context.annotation.Bean
-import org.springframework.http.HttpMethod
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.servlet.LocaleResolver
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor
 import org.springframework.web.servlet.i18n.SessionLocaleResolver
 import java.util.*
 
-
 @EnableWebSecurity
-class SecurityConfig(
-    private val userDetailsService: UserService,
-    private val bCryptPasswordEncoder: BCryptPasswordEncoder) : WebSecurityConfigurerAdapter() {
-
-    companion object SecurityConstants {
-        const val SIGN_UP_URL = "/users"
-        val ALLOWED_URLS = arrayOf(
-            "/authenticate",
-            "/swagger-resources/**",
-            "/swagger-ui/**",
-            "/swagger-ui.html",
-            "/v3/api-docs/**",
-            "/webjars/**")
-        const val KEY = "q3t6w9z\$C&F)J@NcQfTjWnZr4u7x!A%D*G-KaPdSgUkXp2s5v8y/B?E(H+MbQeTh"
-        const val HEADER_NAME = "Authorization"
-        const val EXPIRATION_TIME = 1000L * 60 * 30
-    }
+class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
-        http.cors().and().csrf().disable().authorizeRequests()
-            .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
-            .antMatchers(*ALLOWED_URLS).permitAll()
+        http.csrf().disable()
+            .authorizeRequests()
             .anyRequest().authenticated()
             .and()
-            .addFilter(AuthenticationFilter(authenticationManager()))
-            .addFilter(AuthorizationFilter(authenticationManager()))
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-    }
-
-    @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
-        val source = UrlBasedCorsConfigurationSource()
-        source.registerCorsConfiguration("/**", CorsConfiguration().applyPermitDefaultValues())
-        return source
-    }
-
-    @Throws(Exception::class)
-    override fun configure(auth: AuthenticationManagerBuilder) {
-        auth.userDetailsService<UserDetailsService>(userDetailsService).passwordEncoder(bCryptPasswordEncoder)
+            .oauth2Login()
     }
 
     @Bean
